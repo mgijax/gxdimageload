af4cb8ecee611d8f7429b8b982d347d7f11c6e52
Lori Corbani <Lori.Corbani@jax.org>
2009-01-15 18:09:01 +0000

    gxdimageload-4-2-0-3

---
+++ tr9458fullsize.py
@@ -0,0 +1,180 @@
+#!/usr/local/bin/python
+
+#
+# Program: tr9458fullsize.py
+#
+# Original Author: Lori Corbani
+#
+# Purpose:
+#
+#	To create input files for the gxdimageload.py program.
+#	from full-size of TR9471.
+#
+# Input:
+#
+#       J141291stubs.txt, tab-delimited file in the format:
+#               field 1: file name
+#
+#	PIX_FULLSIZE, tab-delimited list of full size
+#		field 1: file name
+#		field 2: pix id
+#
+# Outputs:
+#
+#       2 tab-delimited files:
+#
+#	image.txt
+#	imagepane.txt
+#	
+#       1 error file:
+#
+#	image.error
+#
+
+import sys
+import os
+import string
+import db
+import mgi_utils
+import jpeginfo
+import assoclib
+
+#
+#  CONSTANTS
+#
+FIRST_IMAGE_FILE_INDEX = 9
+
+CAPTION = '''This image was provided by the authors as a direct GXD submission. Questions regarding this image or its use in publications should be directed to Dr. Heiko Lickert at E-mail: heiko.lickert@helmholtz-muenchen.de.'''
+
+COPYRIGHT = '''This image is from Tamplin OJ, BMC Genomics 2008;9(1):511, an open-access article, licensee BioMed Central Ltd.'''
+
+FULLSIZE_IMAGE_KEY = ''
+
+#
+#  GLOBALS
+#
+pixelDBDir = os.environ['PIXELDBDATA']
+pixFile = os.environ['PIX_FULLSIZE']
+imageListFile = os.environ['IMAGE_LIST_FIG_FILE']
+imageFile = os.environ['IMAGE_FULLSIZE']
+imagePaneFile = os.environ['IMAGEPANE_FULLSIZE']
+jNumber = os.environ['REFERENCE']
+pixelDict = {}
+
+#
+# Purpose: Open the files.
+# Returns: Nothing
+# Assumes: The names of the files are set in the environment.
+# Effects: Sets global variables
+# Throws: Nothing
+#
+def openFiles ():
+    global fpImageList, fpImageFile, fpImagePaneFile, pixelDict
+
+    #
+    # Open the input file.
+    #
+    try:
+        fpImageList = open(imageListFile, 'r')
+    except:
+        sys.stderr.write('Cannot open input file: ' + imageListFile + '\n')
+        sys.exit(1)
+
+    try:
+        fpPixFile = open(pixFile, 'r')
+    except:
+        sys.stderr.write('Cannot open input file: ' + pixFile + '\n')
+        sys.exit(1)
+
+    #
+    # Open the output files.
+    #
+    try:
+        fpImageFile = open(imageFile, 'w')
+    except:
+        sys.stderr.write('Cannot open output file: ' + imageFile + '\n')
+        sys.exit(1)
+
+    try:
+        fpImagePaneFile = open(imagePaneFile, 'w')
+    except:
+        sys.stderr.write('Cannot open output file: ' + imagePaneFile + '\n')
+        sys.exit(1)
+
+    pixelDict = assoclib.readPixelFile(fpPixFile)
+
+    return
+
+
+#
+# Purpose: Close the files.
+# Returns: Nothing
+# Assumes: Nothing
+# Effects: Nothing
+# Throws: Nothing
+#
+def closeFiles ():
+    fpImageList.close()
+    fpImageFile.close()
+    fpImagePaneFile.close()
+
+    return
+
+
+#
+# Purpose: Create the image and image pane output files for each pixel DB
+#          image that is being added.
+# Returns: 0 if successful, 1 for an error
+# Assumes: Nothing
+# Effects: Nothing
+# Throws: Nothing
+#
+def process ():
+
+    #
+    # Search through each line of the image list file.
+    #
+    lineNum = 0
+    for line in fpImageList.readlines():
+
+	lineNum = lineNum + 1
+
+	if lineNum == 1:
+	    continue
+
+        tokens = string.split(line[:-1], '\t')
+        filename =  tokens[0]
+        (figureLabel, j) = string.split(filename, '.jpg')
+
+	if not pixelDict.has_key(filename):
+        	print 'Cannot find filename: ' + filename + '\n'
+		continue
+		
+        pixID = pixelDict[filename]
+
+        #
+        # Get the X an Y dimensions of the image file.
+        #
+        (xdim, ydim) = jpeginfo.getDimensions(pixelDBDir + '/' + pixID + '.jpg')
+
+        fpImageFile.write(jNumber + '\t' +
+                          FULLSIZE_IMAGE_KEY + '\t' +
+                          pixID + '\t' +
+                          str(xdim) + '\t' +
+                          str(ydim) + '\t' +
+                          figureLabel + '\t' +
+                          COPYRIGHT + '\t' +
+                          CAPTION + '\n')
+
+        fpImagePaneFile.write(pixID + '\t' + '\n')
+
+    return 0
+
+#
+# Main
+#
+openFiles()
+process()
+closeFiles()
+
+sys.exit(0)
